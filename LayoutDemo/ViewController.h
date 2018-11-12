9de67487b8bb342df6efcb32dbc6510209580aa7
黄洪彬 <huanghongbin@corp.netease.com>
2018-11-12 10:50:22 +0800

    Initial Commit

---
+++ LayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayoutDemo
+//
+//  Created by 黄洪彬 on 2018/11/12.
+//  Copyright © 2018 黄洪彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LayoutDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LayoutDemo
+//
+//  Created by 黄洪彬 on 2018/11/12.
+//  Copyright © 2018 黄洪彬. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
